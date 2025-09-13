6b47009ef22ca02d89941bada522cb260fc4fff7
Ushasandhiya <sandhiya.pcy@gmail.com>
2025-09-13 23:40:18 +0530

    css practise project created

---
+++ functions&scopes.js
@@ -0,0 +1,34 @@
+let globalVar = "this is global";
+function greet(name)
+{
+    let message = "hello" + name ;
+    return message
+}
+
+const add = (a,b) => a+b;
+ function outerfunction(outervar)
+ {
+    return function
+    innerFunction(innervar)
+    {
+        return outerfunction  ;
+    }
+ }
+
+
+ console.log(globalVar);
+
+ console.log(greet("sandhiya"));
+
+ console.log("sum=" + add(5,3));
+
+ const closurexp = outerfunction("outside");
+ console.log(closurexp("inside"));
+
+ if(true)
+ {
+    let blockvar = "inside block";
+    console.log(blockvar);
+ }
+
+ 
